58155dfb3e9f86a00907de260f3da5f593171508
Guil <guil.levy12@gmail.com>
2024-11-09 13:29:11 +0200

    1

--- Diver exercices/Exercice reverse sentence/ex1.js
+++ Diver exercices/Exercice reverse sentence/ex1.js
@@ -1,1 +1,18 @@
-console.log("Hello beautiful world")
+// EX1 ---------------------------------------------
+
+let sentence = "Hello beautiful world !";
+console.log(sentence);
+
+// SPLIT SANS ESPACE ("")= split chaque caractere 
+let reversedSentence = sentence.split("").reverse().join("");
+
+console.log(reversedSentence);
+
+function reverse(str){
+    console.log(str.split('').reverse().join(''));
+}
+
+const string = "I want to live in America"
+
+reverse(string)
+reverse(sentence)
